067aac2143c9983b7fcb0844e528b0b868d6466a
AdenJWong <62776412+AdenJWong@users.noreply.github.com>
2022-01-22 17:37:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("main")
+import pygame, sys
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screen_widgth = 1280
+screen_height = 960
+screen = pygame.display.set_mode((screen_widgth, screen_height))
+pygame.display.set_caption('pong')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.flip()
+    clock.tick(60)
